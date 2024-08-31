ccc0baa4d12a3e78a889d10b1b32696a85838c85
Andrii Yevchenko <and.yevchenko@gmail.com>
2024-08-31 22:24:03 +0300

    Add basic stepper

--- src/components/Stepper/Stepper.jsx
+++ src/components/Stepper/Stepper.jsx
@@ -1,15 +1,19 @@
+import { useState } from 'react';
 import { TRAVEL_PLAN } from '../../content/__travel-plan'
 import './Stepper.css'
 
 export const Stepper = () => {
     
     const steps = TRAVEL_PLAN.ITINERARY;
+    const [currentStep, setCurrentStep] = useState(1)
 
     return (
         <div className='stepper'>
             {steps.map((step, i) => (
-                <div className='stepper__item' key={i}>
-                    <div></div>
+                <div className={`stepper__item ${currentStep >= i + 1 && '--active'}`} 
+                     key={i}
+                >
+                    <button onClick={() => setCurrentStep(i + 1)} type='button'></button>
                     <p>{step}</p>
                 </div>
             ))}
