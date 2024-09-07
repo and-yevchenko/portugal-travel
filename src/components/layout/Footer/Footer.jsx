6f76f1dd12279cd1b7883bc44629a5d0c203debb
Andrii Yevchenko <and.yevchenko@gmail.com>
2024-09-07 20:11:57 +0300

    Update footer styles & add _info.js

--- src/components/layout/Footer/Footer.jsx
+++ src/components/layout/Footer/Footer.jsx
@@ -1,35 +1,34 @@
-import { Logo } from '../../Logo/Logo'
+import { INFO } from '../../../content/__info'
 import './Footer.css'
 
 export const Footer = () => {
+
+    const INFO_FOOTER = INFO
 
     return (
         <footer className='footer'>
             <div className='footer__up'>
                 <div className='footer__company'>
-                    <a className='footer__name' href="#">Horizon</a>
+                    <a className='footer__name' href="#">{INFO_FOOTER.NAME}</a>
                     <div className='footer__contacts'>
-                        <span>Address:</span>
-                        <span>Phone:</span>
-                        <span>Email:</span>
+                        <address>Address: {INFO_FOOTER.ADDRESS}</address>
+                        <span>Phone: <a href={`tel:${INFO_FOOTER.PHONE}`}>{INFO_FOOTER.PHONE}</a></span>
+                        <span>Email: <a href={`mailto:${INFO_FOOTER.EMAIL}`}>{INFO_FOOTER.EMAIL}</a></span>
                     </div>
                 </div>
                 <div className='footer__links'>
                     <a href="#">Privacy Policy</a>
                     <a href="#">Booking and Cancellation Policy</a>
                     <div className='footer__social'>
-                        <a href="#" title='Facebook'></a>
-                        <a href="#" title='Instagram'></a>
-                        <a href="#" title='X'></a>
-                        <a href="#" title='LinkedIn'></a>
+                        {INFO_FOOTER.SOCIAL.map((el) => (
+                            <a key={el.id} href="#" title={el.title}><img src={el.icon} alt={el.title} /></a>
+                        ))}
                     </div>
-                </div>
-                <div className='footer__logo'>
-                    <Logo />
                 </div>
             </div>
             <div className='footer__down'>
-                <p>© 2024 Horizon. <a href="https://github.com/and-yevchenko" target='_blank'>and-yevchenko</a>. All rights reserved.</p>
+                <p>© 2024 {INFO_FOOTER.NAME}. <a href="https://github.com/and-yevchenko" target='_blank'>
+                {INFO_FOOTER.AUTHOR}<img src="/public/icons/github.svg" alt="GitHub"/></a>. All rights reserved.</p>
             </div>
         </footer>
     )
---
+++ src/content/__info.js
@@ -0,0 +1,32 @@
+
+export const INFO = {
+    NAME: "Horizon",
+    ADDRESS: "Rua da Liberdade, 123 1200-456 Lisboa, Portugal",
+    PHONE: "+351 21 234 5678",
+    EMAIL: "info@portugalhorizon.pt",
+    AUTHOR: "and-yevchenko",
+
+    SOCIAL: [
+        {
+            id: 1,
+            title: "Facebook", 
+            icon: "/public/icons/facebook.svg"
+        },
+        {
+            id: 2,
+            title: "Instagram", 
+            icon: "/public/icons/instagram.svg"
+        },
+        {
+            id: 3,
+            title: "X", 
+            icon: "/public/icons/x.svg"
+        },
+        {
+            id: 4,
+            title: "LinkedIn", 
+            icon: "/public/icons/linkedin.svg"
+        },
+    ],
+
+}
